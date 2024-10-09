e442f39f0adcc4f5991bba8d83d1a358ffd8baca
eduardscutariu <scutariueduard22@stud.ase.ro>
2024-10-09 17:35:06 +0300

    variables

---
+++ test.js
@@ -0,0 +1,24 @@
+function test()
+{
+    if(true)
+    {
+        var var1=1;
+        let let2=2;
+        const const3=3;
+    }
+
+    console.log(var1);
+    console.log(let2);
+    console.log(const3);
+}
+
+// test();
+
+const obj={};
+// obj.name='Adrian'
+obj={}
+
+// console.log(obj)
+const arr=[1,2,3,4]
+arr.push(5)
+console.log(arr)
