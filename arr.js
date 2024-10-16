d5cd037ddfe5d50bc2665d0b8b98736b387f19b5
eduardscutariu <scutariueduard22@stud.ase.ro>
2024-10-16 16:43:54 +0300

    seminar2

---
+++ arr.js
@@ -0,0 +1,18 @@
+const arr=[1,2,3,4,5]
+
+for( let i=0;i<arr.length;i++)
+{
+    console.log(arr[i]);
+}
+
+for(el of arr)
+{
+    console.log(el);
+}
+
+for(el in arr)
+{
+    console.log(el);
+}
+
+  
---
+++ functions.js
@@ -0,0 +1,12 @@
+function sum1(a,b)
+{
+    console.log(a+b);
+}
+
+const sum2=(a,b)=>
+{
+    console.log(a+b);
+}
+
+sum1(1,2);
+sum2(1,2);
